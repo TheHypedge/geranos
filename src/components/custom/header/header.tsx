18ae9d1e95d1965757a43db025afcf2128ac245d
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-21 11:27:42 +0530

    Changes
    Changes

--- src/components/custom/header/header.tsx
+++ src/components/custom/header/header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import MainNavigation from './mainNavigation'
 
 const Header = () => {
   return (
     <div className="flex flex-row align-middle justify-between px-4 py-2">
     <Image src="/global/logo.png" width="200" height="200" alt="logo"/>        
-    <div className="flex align-middle">Header</div>
+    <MainNavigation/>
 
     </div>
   )
--- src/components/custom/heroBanner/heroBanner.tsx
+++ src/components/custom/heroBanner/heroBanner.tsx
@@ -5,6 +5,7 @@ import { HTMLAttributes, useEffect, useState } from "react";
 import WaveReveal from "@/components/animata/text/wave-reveal";
 import { cn } from "@/lib/utils";
 
+
 interface ImageProps extends HTMLAttributes<HTMLDivElement> {
   item: { image: string; title: string };
   index: number;
@@ -39,8 +40,8 @@ const List = ({ item, className, index, activeItem, ...props }: ImageProps) => {
       {index === activeItem && (
         <div className="absolute bottom-4 left-4 min-w-fit text-white md:bottom-8 md:left-8">
           <WaveReveal
-            duration="1000ms"
-            className="items-start justify-start text-xl sm:text-2xl md:text-6xl"
+            duration="500ms"
+            className="items-start justify-start text-xl sm:text-2xl md:text-6xl font-semibold px-0"
             text={item.title}
             direction="up"
           />
@@ -53,8 +54,8 @@ const List = ({ item, className, index, activeItem, ...props }: ImageProps) => {
 const items = [
   {
     image:
-      "https://images.unsplash.com/photo-1541753236788-b0ac1fc5009d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3",
-    title: "Mountains",
+      "https://images.unsplash.com/photo-1652604244093-1a06aca55929?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Golden Temple",
   },
   {
     image:
