31457c8148ca5589b69ae68737a7f6b6b49b6e34
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-21 21:11:32 +0530

    Changes
    Changes

--- src/components/custom/footer/footer.tsx
+++ src/components/custom/footer/footer.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
+import { Separator } from "@/components/ui/separator"
 
 
 const Footer = () => {
   return (
-    <div className="border-b-[10px] border-[#EB1D25] w-full bg-white min-h-[90px] flex flex-row align-middle justify-between px-4 py-2">
-  
+    <>
+    <div className="border-b-[10px] border-[#9d1116] w-full bg-[#EC1C26] text-white min-h-[90px] flex flex-col justify-around px-4 py-2 pt-12 pb-12">
+        <div className="flex flex-row">
+            <p className="text-2xl">The Travel Compnay: We are great planners!</p>
+            <p className="text-2xl">WhatsApp</p>
 
+        </div>
+        <Separator />
+        <div className="flex flex-row">        
+
+        <p className="text-2xl">The Travel Compnay: We are great planners!</p>
+        <p className="text-2xl">WhatsApp</p>
+        </div>
     </div>
+ 
+
+    </>
   )
 }
 
