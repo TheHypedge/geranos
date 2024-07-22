e03bd6089028acea8c2bc213b7818ec34658d54b
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-07-22 10:11:57 +0530

    Changes
    Changes

--- src/components/custom/footer/footer.tsx
+++ src/components/custom/footer/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <>
     <div className="border-b-[10px] border-[#9d1116] w-full bg-[#EC1C26] text-white min-h-[90px] flex flex-col justify-around px-4 py-2 pt-12 pb-12">
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-between">
             <p className="text-2xl">The Travel Compnay: We are great planners!</p>
             <p className="text-2xl">WhatsApp</p>
 
