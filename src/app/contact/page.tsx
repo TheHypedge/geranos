896c0750ed0afb65b055c1f44fcb1e4c3d7103f2
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-07-29 12:57:50 +0530

    Changes
    Changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className="min-h-[400px]">Page</div>
+    <div className="min-h-[400px] flex flex-col my-12">
+            <div className="flex aling-middle justify-center gap-4 text-center min-h-[260px] flex-col px-4 py-12 bg-slate-50">
+                <h1 className="text-4xl font-semibold">Contact</h1>
+                <p>Sub Heading</p>
+            </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
