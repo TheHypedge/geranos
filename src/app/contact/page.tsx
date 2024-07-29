c8a890f9ad64c8ee61f6860a065289111b134c73
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-07-29 13:05:50 +0530

    Changes
    Changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,11 +4,16 @@ const Page = () => {
   return (
     <div className="min-h-[400px] flex flex-col my-12">
             <div className="flex aling-middle justify-center gap-4 text-center min-h-[260px] flex-col px-4 py-12 bg-slate-50">
-                <h1 className="text-4xl font-semibold">Contact</h1>
-                <p>Sub Heading</p>
+                <h1 className="text-4xl font-semibold">Contact Us</h1>
+                <p>We are happy to help!</p>
             </div>
 
+            <div className="max-w-[600px] m-auto w-full p-4 bg-slate-50">
 
+
+test
+
+            </div>
 
 
 
--- src/components/custom/footer/footer.tsx
+++ src/components/custom/footer/footer.tsx
@@ -8,15 +8,28 @@ const Footer = () => {
     <>
     <div className="border-b-[10px] border-[#9d1116] w-full bg-[#EC1C26] text-white min-h-[90px] flex flex-col justify-around px-4 py-8 pt-12 pb-12">
         <div className="flex flex-row justify-between mb-12">
-            <p className="text-2xl">The Travel Compnay: We are great planners!</p>
-            <p className="text-2xl">WhatsApp</p>
+            <p className="text-xl mt-2">The Travel Compnay: We are great planners!</p>
+            <p className="flex gap-2">
+              <Link href="https://x.com">
+                <Image src="/global/social/x.png" alt="Social" width="40" height="40"/>
+              </Link>
+              <Link href="https://facebook.com">
+                <Image src="/global/social/facebook.png" alt="Social" width="40" height="40"/>
+              </Link>
+              <Link href="https://instagram.com">
+                <Image src="/global/social/instagram.png" alt="Social" width="40" height="40"/>
+              </Link>
+              <Link href="https://linkedin.com">
+                <Image src="/global/social/linkedin.png" alt="Social" width="40" height="40"/>
+              </Link>
+            </p>
 
         </div>
         <Separator className="mb-12"/>
         <div className="flex flex-row justify-between">        
 
         <Link href="/contact">
-        <p className="text-2xl">Contact</p>
+        <p className="text-xl">Contact</p>
         </Link>
 
         <p className="text-2xl">WhatsApp</p>
