11a198b51d5b36a0c4eee4a9bb85e9236bd77f7f
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-07-31 16:47:52 +0530

    Changes
    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@
 import Image from "next/image";
 import HeroBanner from '@/components/custom/HeroBanner/heroBanner'
 import TextReveal from "@/components/custom/TextReveal/TextReveal";
+import TextAndImage from "@/components/custom/Content/TextAndImage/TextAndImage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <HeroBanner/>
-     
+      <TextAndImage/>     
     </main>
   );
 }
---
+++ src/components/custom/Content/TextAndImage/TextAndImage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TextAndImage = () => {
+  return (
+    <div className="max-w-4xl flex m-auto w-full">
+        <div className="flex gap-4 flex-row justify-between w-full">
+            <div className="bg-black w-full">
+                test1
+            </div>
+            <div className="bg-black w-full">
+                test2
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TextAndImage
