3f3a2a662eda2099b7c48e24988c48791e4c5a53
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-07-31 18:32:40 +0530

    Changes
    Changes

--- src/components/custom/Content/TextAndImage/TextAndImage.tsx
+++ src/components/custom/Content/TextAndImage/TextAndImage.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import Heading from './Heading'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 const TextAndImage = () => {
   return (
-    <div className="max-w-4xl flex m-auto w-full">
-        <div className="flex gap-4 flex-row justify-between w-full">
-            <div className="bg-black w-full">
-                test1
+    <div className="max-w-[1200px] flex m-auto w-full">
+        <div className="flex gap-4 flex-col justify-between w-full xl:flex-row py-12 px-4">
+            <div className="w-full flex flex-col align-middle justify-center gap-8">
+                <Heading content="Gerenos Getaways"/>
+                <p>Discover Punjab with our tourism website, your guide to its rich heritage and scenic beauty. Explore top attractions like the Golden Temple, Shivalik hills, and more. Get curated itineraries, insider tips, and detailed guides for an authentic experience. Plan your perfect Punjab trip with us!</p>
+                <Button className="max-w-min">Read More</Button>
             </div>
-            <div className="bg-black w-full">
-                test2
+            <div className="w-full">
+                <Image src="/global/collage-1.png" width="800" height="800" alt="Image"/>
             </div>
         </div>
     </div>
