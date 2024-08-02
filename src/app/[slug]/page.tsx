d274749b053977a7e8043ac401c8f8bb7d351f96
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-08-02 17:01:45 +0530

    Changes
    Changes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,25 +1,25 @@
+
+
 import { Button } from '@/components/ui/button';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const revalidate = 1
 
 const Pages = async({ params } : {params:any}) => {
 
-    const req = await fetch(`https://grhealthy.in/wp-json/wp/v2/pages?_fields=id,slug,title,content&slug=${params.slug}', { next: { revalidate: 1 } }`); 
+    const req = await fetch(`https://grhealthy.in/wp-json/wp/v2/pages?_fields=id,slug,title,content&slug=${params.slug}`); 
     const pages = await req.json(); 
     const page = pages[0];
-
 
 
   return (
     <div className="max-w-[800px] m-auto px-2">
 
-      {}
+ 
+            <div className="text-4xl text-left font-bold py-[80px]">test{page.title.rendered}</div>
+            <div className="postContParaFixCstm mb-2 text-lg font-semibold text-slate-900" dangerouslySetInnerHTML={{__html: page.content.rendered}}/>
 
-
-            <div className="text-4xl text-left font-bold py-[80px]">{page.title.rendered}</div>
-            <div className="postContParaFixCstm mb-2 text-lg font-semibold text-slate-900" dangerouslySetInnerHTML={{__html: page.content.rendered}}/>  
-       
+              
   
     </div>    
   )
