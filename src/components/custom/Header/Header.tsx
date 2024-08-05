e3e1ffac26be1dbe5bf463e8d67df86c8dc82089
Himanshu Kashyap <hkashyap.connect@gmail.com>
2024-08-05 22:27:32 +0530

    Changes
    changes

--- src/components/custom/Header/Header.tsx
+++ src/components/custom/Header/Header.tsx
@@ -4,7 +4,7 @@ import MainNavigation from './mainNavigation'
 
 const Header = () => {
   return (
-    <div className="border-t-[10px] border-[#246BEB] fixed top-0 z-50 w-full bg-white min-h-[90px] flex flex-row align-middle justify-between px-4 py-2">
+    <div className="border-t-[10px] border-[#246BEB] fixed top-0 z-50 w-full bg-white min-h-[90px] flex flex-row align-middle justify-between px-6 py-2">
     <Image src="/global/logo.png" width="200" height="200" alt="logo"/>        
     <MainNavigation/>
 
