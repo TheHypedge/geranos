125d12c16de854f48ab4c536809ffce2dac2f2d4
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-08-08 17:17:52 +0530

    Changes
    Changes

--- src/components/custom/Global/HeroHomeSlider/HeroHomeSlider.tsx
+++ src/components/custom/Global/HeroHomeSlider/HeroHomeSlider.tsx
@@ -19,8 +19,6 @@ export default function SimpleSlider() {
 
 
 
-    let sliderRef = useRef(null);
-
 
 
 
@@ -43,9 +41,7 @@ export default function SimpleSlider() {
     <>
 
     <Slider 
-    ref={slider => {
-        sliderRef = slider;
-      }}
+
     {...settings}>
 
 
