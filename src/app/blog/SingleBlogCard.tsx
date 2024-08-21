283adea142956a87b8eef7869f906932d7086d9d
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-08-21 08:01:06 +0530

    changes
    changes

--- src/app/blog/SingleBlogCard.tsx
+++ src/app/blog/SingleBlogCard.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Image from 'next/image'
 import {Button} from '@/components/ui/button'
 import Link from 'next/link'
+
 const SingleBlogCard = ({title, date, excerpt, slug, image}:any) => {
+
+
+  let d = new Date(date);
+  
   return (
     <div className="flex flex-col md:flex-row max-w-6xl m-auto py-8 px-4 sm:px-8 gap-8 border-b-2">
 
         <div className="flex flex-col gap-4 align-middle justify-center">
             <h2 className="text-xl md:text-3xl font-bold">{title}</h2>
-            <p className="text-slate-400 text-sm md:text-md">{date}</p>
+            <p className="text-slate-400 text-sm md:text-md">{d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear()}</p>
             <div className="text-md md:text-md xl:text-xl"
             dangerouslySetInnerHTML={{__html: excerpt}}
             />
@@ -18,7 +23,7 @@ const SingleBlogCard = ({title, date, excerpt, slug, image}:any) => {
         </div>
 
         <div className="flex flex-col order-[-1] md:order-1 gap-4 align-middle justify-center md:min-w-[280px] xl:min-w-[320px]">
-            <Image src={image} width="400" height="400" alt="blog thumbnail" className="m-auto rounded-xl"/>
+            <Image src={image} width="400" height="400" alt="blog thumbnail" className="m-auto rounded-xl mx-h-[200px] sm:max-h-full"/>
         </div>
 
 
