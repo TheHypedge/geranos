864812c1d2eb352f29c4c57ce3e54053d4ed8038
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-08-22 13:20:09 +0530

    changes
    changes

--- src/app/[slug]/PageRenderModule.tsx
+++ src/app/[slug]/PageRenderModule.tsx
@@ -3,21 +3,44 @@
 import React from 'react'
 import { useEffect } from 'react';
 
+import Link from 'next/link'
+import {Button} from '@/components/ui/button'
+import Image from 'next/image'
+
+
 const PageRenderModule = (props: any) => {
 
- let test = props.pageData
+
 
 
 
   return (       
 
-        <div className="max-w-[800px] m-auto px-2">
+<>
 
-        <div className="text-4xl text-left font-bold py-[80px]">{props.pageData.title.rendered}</div>
+
+    <Image src={props.pageData.acf.featured_image} width="1920" height="800" alt="Featured Image" className="absolute max-h-[420px] object-cover bg-cover  min-h-[420px] bg-slate-50"/>
+    <div className="bg-[rgba(0,0,0,0.3)] flex aling-middle justify-center gap-6 text-center flex-col min-h-[420px] px-4 py-12 z-10">
+    <h1 className="text-white text-5xl font-semibold">{props.pageData.title.rendered}</h1>
+    <p className="text-white text-xl">We are happy to assist journeys and life experiences!</p>
+    
+
+    </div>
+
+
+
+
+
+
+
+        <div className="max-w-[800px] m-auto px-2 pt-16 pb-8">
+
+      
         <div className="postContParaFixCstm text-left" dangerouslySetInnerHTML={{__html: props.pageData.content.rendered}}/>
 
  
     </div>
+    </>
   )
 }
 
