309ef45e1e6203ead3f0a6785f1d99e50a61fbd1
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-08-22 13:38:17 +0530

    changes
    changes

--- src/app/[slug]/PageRenderModule.tsx
+++ src/app/[slug]/PageRenderModule.tsx
@@ -19,10 +19,10 @@ const PageRenderModule = (props: any) => {
 <>
 
 
-    <Image src={props.pageData.acf.featured_image} width="1920" height="800" alt="Featured Image" className="absolute max-h-[420px] object-cover bg-cover  min-h-[420px] bg-slate-50"/>
-    <div className="bg-[rgba(0,0,0,0.3)] flex aling-middle justify-center gap-6 text-center flex-col min-h-[420px] px-4 py-12 z-10">
-    <h1 className="text-white text-5xl font-semibold">{props.pageData.title.rendered}</h1>
-    <p className="text-white text-xl">We are happy to assist journeys and life experiences!</p>
+
+    <div className="bg-[rgba(0,0,0,0.3)] flex aling-middle justify-center gap-6 text-center flex-col max-h-[320px] sm:max-h-[420px] h-full z-10">
+    <Image src={props.pageData.acf.featured_image} width="1920" height="800" alt="Featured Image" className="absolute  max-h-[320px] sm:max-h-[420px] h-full w-full object-cover bg-cover   bg-slate-50"/>
+
     
 
     </div>
@@ -33,13 +33,13 @@ const PageRenderModule = (props: any) => {
 
 
 
-        <div className="max-w-[800px] m-auto px-2 pt-16 pb-8">
+        <div className="flex flex-col gap-8 max-w-[800px] m-auto px-2 pt-16 pb-8">
 
-      
+        <h1 className="text-5xl font-semibold">{props.pageData.title.rendered}</h1>
         <div className="postContParaFixCstm text-left" dangerouslySetInnerHTML={{__html: props.pageData.content.rendered}}/>
 
  
-    </div>
+      </div>
     </>
   )
 }
