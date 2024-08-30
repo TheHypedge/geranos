f1926ba4533508cfaa8b9ea723a94bc9c5251a01
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-08-30 17:53:09 +0530

    changes
    changes

--- src/components/custom/Content/TextAndImage/TextAndImageText.tsx
+++ src/components/custom/Content/TextAndImage/TextAndImageText.tsx
@@ -8,9 +8,13 @@ const TextAndImageText = (props:any) => {
     <div className="text-center md:text-left w-full flex flex-col align-middle justify-center gap-8 px-0 md:px-20">
     <h2 className="text-2xl md:text-5xl font-bold text-[#246BEB] font-EduVICWANTBeginner">{props.titleText}</h2>
     <h3 className="text-xl md:text-2xl text-slate-700 font-bold font-UrbanistLight">{props.subHeading}</h3>
-    <p className="text-justify text-sm sm:text-base font-UrbanistLight tracking-wide">{props.content}</p>
-    
-    
+
+    <div
+      className="text-justify text-sm sm:text-xl font-UrbanistLight tracking-wide"
+      dangerouslySetInnerHTML={{__html: props.content}}
+    />
+
+
     { props.ctaLink &&
       <div className="text-center">
       <AlgoliaBlueButton ctaText={props.ctaText} ctaLink={props.ctaLink}/>
