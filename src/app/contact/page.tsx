5c05e5537c92d062b509656675ba6b072984ae0b
Himanshu Kashyap <sashank@geranosgetaways.com>
2024-09-19 23:16:06 +0530

    c
    c

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,15 +28,22 @@ const Page = () => {
             <ul className="flex gap-4 text-left flex-col align-middle justify-center">
                 <li className="text-slate-700 flex flex-row gap-2 justify-start align-middle text-md sm:text-xl font-regular">
                     <PhoneIcon className="h-5 w-5 sm:h-6 sm:w-6 mt-1" />
-                    +91 81466 59314
+                    <Link href="tel:+918837562970">
+                    +91 88375 62970
+                    </Link>
                     </li>
                 <li className="text-slate-700 flex flex-row gap-2 justify-start align-middle text-md sm:text-xl font-regular">
                     <MailIcon className="h-5 w-5 sm:h-6 sm:w-6 mt-1" />
-                    hello@gerenosgetaways.com
+                    <Link href="mailto:info@geranosgetaways.com">
+                    info@geranosgetaways.com
+                    </Link>
                     </li>
                 <li className="text-slate-700 flex flex-row gap-2 justify-start align-middle text-md sm:text-xl font-regular">
                     <WhatsappIcon className="h-5 w-5 sm:h-6 sm:w-6 mt-1" />
+                    <Link href="https://wa.link/601xde" target="_blank">
                     WhatsApp Us
+                    </Link>
+                    
                     </li>
             </ul>
           </div>
